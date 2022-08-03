68384440588e746fb2e6f2a6a48e83a512748beb
Hallison Melo <hallison.pm@gmail.com>
2022-08-03 15:51:01 -0300

    service baseurl

--- client/src/service/index.ts
+++ client/src/service/index.ts
@@ -2,18 +2,18 @@ import router from "@/router";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.VUE_APP_BASE_URL,
-    timeout: 60000
+  baseURL: "http://localhost:3000/",
+  timeout: 60000,
 });
 
 api.interceptors.response.use(
-    config => config,
-    error => {
-        if (error.response.status === 401) {
-            router.push("/logout");
-        }
-        return Promise.reject(error.response);
+  (config) => config,
+  (error) => {
+    if (error.response.status === 401) {
+      router.push("/logout");
     }
+    return Promise.reject(error.response);
+  }
 );
 
 export default api;
